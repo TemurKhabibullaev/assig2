662ce0d23f3c8d50f44fe8b41f4ae125a7f4c434
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-02-11 00:52:45 -0500

    done

--- drive.py
+++ drive.py
@@ -1,6 +1,53 @@
 #from sep_file
 from sep_file import *
 
-instance = Engine()
-print(instance.lar())
+
+while True:
+    instance = Engine()
+    print("""
+Choose option (DIGITS ONLY):
+0 EXIT
+1 Sort the list
+2 Sum of the list
+3 Product of the list
+4 Mean of the list
+5 Median of the list
+6 Mode of the list
+7 Duplicate removal
+8 Largest number in the list
+9 Smallest number in the list
+10 List without even numbers
+11 List without odd numbers
+12 Check if your number exists in the list
+13 Second largest number""")
+    option = int(input(">>>"))
+    if option == 1:
+        instance.sort()
+    if option == 2:
+        instance.sum()
+    if option == 3:
+        instance.product()
+    if option == 4:
+        instance.mean()
+    if option == 5:
+        instance.median()
+    if option == 6:
+        instance.mode()
+    if option == 7:
+        instance.dup()
+    if option == 8:
+        instance.lar()
+    if option == 9:
+        instance.sma()
+    if option == 10:
+        instance.eve()
+    if option == 11:
+        instance.odd()
+    if option == 12:
+        instance.exi()
+    if option == 13:
+        instance.sec_lar()
+    if option == 0:
+        print("Bye!")
+        break
 
--- sep_file.py
+++ sep_file.py
@@ -1,9 +1,7 @@
 # Temur Khabibullaev 2/10/2020 This is separate file
-
+# too tired to explain but I know everything what each line does
 
 class Engine:
-
-
     def __init__(self):
         self.first = int(input("Give me a first number:\n>>>"))
         self.second = int(input("Give me a second number:\n>>>"))
@@ -26,12 +24,74 @@ class Engine:
 
     def median(self):
         ascending_order = sorted(self.list)
-        return ascending_order[2]
+        return ascending_order[int(5 / 2)]
 
     def mode(self):
-        new_list = [self.first, self.second, self.third, self.fourth, self.fifth]
-        sorting = sorted(new_list)
+        sorting = sorted(self.list)
+        x = 0
+        if sorting[0] < sorting[1]:
+            pass
+        elif sorting[0] == sorting[1]:
+            x += sorting[0]
+        if sorting[1] < sorting[2]:
+            pass
+        elif sorting[1] == sorting[2]:
+            x += sorting[1]
+        if sorting[2] < sorting[3]:
+            pass
+        elif sorting[2] == sorting[3]:
+            x += sorting[2]
+        if sorting[3] < sorting[4]:
+            pass
+        elif sorting[3] == sorting[4]:
+            x += sorting[3]
+        if sorting[4] == sorting[4]:
+            pass
+        return x, ' is a mode of the list'
 
+    def dup(self):
+        sorting = sorted(self.list)
+        x = 0
+        if sorting[0] < sorting[1]:
+            pass
+        elif sorting[0] == sorting[1]:
+            x += sorting[0]
+        if sorting[1] < sorting[2]:
+            pass
+        elif sorting[1] == sorting[2]:
+            x += sorting[1]
+        if sorting[2] < sorting[3]:
+            pass
+        elif sorting[2] == sorting[3]:
+            x += sorting[2]
+        if sorting[3] < sorting[4]:
+            pass
+        elif sorting[3] == sorting[4]:
+            x += sorting[3]
+        if sorting[4] == sorting[4]:
+            pass
+
+        for f in sorting:
+            if x == sorting[0]:
+                sorting[0] = ''
+                return sorting
+                break
+            if x == sorting[1]:
+                sorting[1] = ''
+                return sorting
+                break
+            if x == sorting[2]:
+                sorting[2] = ''
+                return sorting
+                break
+            if x == sorting[3]:
+                sorting[3] = ''
+                return sorting
+                break
+            if x == sorting[4]:
+                sorting[4] = ''
+                return sorting
+                break
 
     def lar(self):
         ascending_order = sorted(self.list)
@@ -40,6 +100,59 @@ class Engine:
     def sma(self):
         ascending_order = sorted(self.list)
         return ascending_order[0]
+
+    def eve(self):
+        ascending_order = sorted(self.list)
+        if (ascending_order[0] % 2) == 0:
+            print(ascending_order[0], 'is an even number, so say bye to him')
+            ascending_order[0] = ''
+        if (ascending_order[1] % 2) == 0:
+            print(ascending_order[1], 'is an even number, so say bye to him')
+            ascending_order[1] = ''
+        if (ascending_order[2] % 2) == 0:
+            print(ascending_order[2], 'is an even number, so say bye to him')
+            ascending_order[2] = ''
+        if (ascending_order[3] % 2) == 0:
+            print(ascending_order[3], 'is an even number, so say bye to him')
+            ascending_order[3] = ''
+        if (ascending_order[4] % 2) == 0:
+            print(ascending_order[4], 'is an even number, so say bye to him')
+            ascending_order[4] = ''
+        return ascending_order
+
+    def odd(self):
+        ascending_order = sorted(self.list)
+        if (ascending_order[0] % 2) != 0:
+            print(ascending_order[0], 'is an odd number, so say bye to him')
+            ascending_order[0] = ''
+        if (ascending_order[1] % 2) != 0:
+            print(ascending_order[1], 'is an odd number, so say bye to him')
+            ascending_order[1] = ''
+        if (ascending_order[2] % 2) != 0:
+            print(ascending_order[2], 'is an odd number, so say bye to him')
+            ascending_order[2] = ''
+        if (ascending_order[3] % 2) != 0:
+            print(ascending_order[3], 'is an odd number, so say bye to him')
+            ascending_order[3] = ''
+        if (ascending_order[4] % 2) != 0:
+            print(ascending_order[4], 'is an odd number, so say bye to him')
+            ascending_order[4] = ''
+        return ascending_order
+
+    def exi(self):
+        ascending_order = sorted(self.list)
+        num = int(input("What number are you looking for?\n>>>"))
+        if num != ascending_order[0] and num != ascending_order[1] and num != ascending_order[2] and num != ascending_order[3] and num != ascending_order[4]:
+            return "No, we don\'t know him"
+        else:
+            return "Yeah, he is with us"
+
+    def sec_lar(self):
+        ascending_order = sorted(self.list)
+        return ascending_order[3]
+
+
+
 
 
 
